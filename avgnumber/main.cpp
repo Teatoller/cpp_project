a439bbee7de4b613126a24fb6ae1d801fc20ca9a
Teatoller <steven.otieno1@gmail.com>
2021-03-31 08:53:18 +0300

    (flow average): Implement average computation
    - compute average of random 15 numbers
    
    [Starts #007]

---
+++ avgnumber/main.cpp
@@ -0,0 +1,36 @@
+/*Find the min and max and average of 15 numbers that a user will input.
+**The numbers range from 0 to 100. 
+*/
+
+#include "main.hpp"
+
+int main()
+{
+    int userInput = 0;
+    int maxNumber = 0;
+    int minNumber = 100;
+    int sumTotal = 0;
+    float average = 0;
+
+    //get the numbers from the user
+    for (int i = 0; i < 15; i++)
+    {
+        std::cout << "Enter a number: ";
+        std::cin >> userInput;
+        std::cout << userInput << "\n";
+        if (userInput > maxNumber)
+        {
+            maxNumber = userInput;
+        }
+        if (userInput < minNumber)
+        {
+            minNumber = userInput;
+        }
+        sumTotal = sumTotal + userInput;
+    }
+    std::cout << "Maximum number = " << maxNumber << "\n";
+    std::cout << "Minimum number = " << minNumber << "\n";
+    average = sumTotal / 15.0;
+    std::cout << "Average = " << average << "\n";
+    return 0;
+}
