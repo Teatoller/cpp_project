f0417b47b248765fe00b514b3f498acccda2f1f8
Teatoller <steven.otieno1@gmail.com>
2021-04-01 14:13:30 +0300

    (function): Implement a sum function
    - add method to find sum
    
    [Starts #009]

---
+++ sumqz/main.cpp
@@ -0,0 +1,14 @@
+/*Goal: practice writing functions with parameters and return values.*/
+#include "main.hpp"
+using namespace std;
+
+int main()
+{
+
+    int m1 = 2;
+    int m2 = 3;
+
+    printEquation(m1, m2, sum(m1, m2), '+');
+
+    return 0;
+}
